046da0933d2e8992d1f6f61fe4ec51947d76d160
Yan Shiuh <yanshiuh12372@gmail.com>
2022-03-09 17:33:55 +0800

    2

---
+++ University_WebPage/main.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelector("#navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
---
+++ University_WebPage/pMain.js
@@ -0,0 +1,9 @@
+const navBar = document.querySelector(".navBar");
+
+function showMenu() {
+    navBar.style.right = "0";
+}
+
+function hideMenu() {
+    navBar.style.right = "-200px";
+}
